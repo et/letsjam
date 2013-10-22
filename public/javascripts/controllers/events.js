7fb09a1d5b6082907d66dd8186d52f14b0570847
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-10-22 17:41:52 -0600

    remove map

--- public/javascripts/controllers/events.js
+++ public/javascripts/controllers/events.js
@@ -1,27 +1,7 @@
 angular.module('letsjam.events').controller('EventsController', ['$scope', 'Events', function ($scope, Events) {
-
-  var markers = [];
-
   $scope.find = function(query) {
     Events.query(query, function(events) {
       $scope.events = events;
-      angular.forEach(events, function(value, key) {
-        var venue = value.venue;
-        markers.push({
-          lat: parseFloat(venue.location["geo:point"]["geo:lat"])
-        , lng: parseFloat(venue.location["geo:point"]["geo:long"])
-        , message: value.title
-        });
-      });
     });
   };
-
-  angular.extend($scope, {
-    boulder: {
-      lat: 40.02931
-    , lng: -105.239977
-    , zoom: 10
-    }
-  , markers: markers
-  });
 }]);
