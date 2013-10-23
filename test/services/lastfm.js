5bbb48e9545ef2e2caacc6e982980327b14a19a8
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-10-22 18:06:58 -0600

    fix tests

--- test/services/lastfm.js
+++ test/services/lastfm.js
@@ -1,15 +1,25 @@
-var expect = require('expect.js');
-var lastfm = require('../../app/services/lastfm');
-var getEvents = require('../fixtures/geo-getevents');
+var expect  = require('expect.js')
+  , request = require('request')
+  , sinon   = require('sinon')
+  , lastfm  = require('../../app/services/lastfm')
+  , events  = require('../fixtures/geo-getevents');
 
 describe("Lastfm Service", function() {
-  describe('geo.getevents', function() {
-    it('should print the results', function(done) {
-      lastfm.get('geo.getevents', function(result) {
-        // TODO - mock this with sinon
-        expect(result).to.eql(getEvents);
-        done();
-      });
+  before(function(done) {
+    sinon.stub(request, 'get').yields(null, null, JSON.stringify(events));
+    done();
+  });
+
+  after(function(done) {
+    request.get.restore();
+    done();
+  });
+
+  it('can retreive a list of events', function(done) {
+    lastfm.get('geo.getevents', function(err, response, result) {
+      if(err) return done(err);
+      expect(result).to.not.be.empty;
+      done();
     });
   });
 });
